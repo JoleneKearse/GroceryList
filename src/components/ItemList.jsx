01ceddab4a2605deecce2eaf205482344b00a0f4
Jolene Kearse <pharaohnutz@gmail.com>
2022-11-05 17:26:22 -0400

    bug: fix JSX bracket error

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -36,36 +36,47 @@ export default function ItemList() {
   }
 
   return (
-    <List py='sm' pb='sm' size="lg">
-      {items.map((item) => (
-        <li
-          className='item'
-          key={item.id}
-          py='sm'
-          pb='sm'
+    <>
+      {items.length ? (
+        <List
+          py="sm"
+          pb="sm"
+          size="lg"
         >
-          <Checkbox
-            type="checkbox"
-            checked={item.checked}
-            color="purple"
-            defaultChecked={false}
-            className=".drac-mr-lg"
-            onChange={(() => handleCheck(item.id))}
-          />
-          <label
-            htmlFor="normal"
-            className="drac-text drac-text-white drac-ml-md"
-            onDoubleClick={() => handleCheck(item.id)}
-            style={(item.checked) ? { textDecoration: "line-through" } : null}
-          >{item.item}</label>
-          <FaTrashAlt
-            onClick={() => handleDelete(item.id)}
-            role="button"
-            stoke="#9580ff"
-            fill="#ff80bf"
-          />
-        </li>
-      ))}
-    </List>
-  );
+          {items.map((item) => (
+            <li
+              className="item"
+              key={item.id}
+              py="sm"
+              pb="sm"
+            >
+              <Checkbox
+                type="checkbox"
+                checked={item.checked}
+                color="purple"
+                defaultChecked={false}
+                className=".drac-mr-lg"
+                onChange={(() => handleCheck(item.id))}
+              />
+              <label
+                htmlFor="normal"
+                className="drac-text drac-text-white drac-ml-md"
+                onDoubleClick={() => handleCheck(item.id)}
+                style={(item.checked) ? { textDecoration: "line-through" } : null}
+              >{item.item}</label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+                stoke="#9580ff"
+                fill="#ff80bf"
+              />
+            </li>
+          ))}
+        </List>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Empty list.</p>
+      )};
+    </>
+  )
 }
